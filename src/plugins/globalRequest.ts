22690f2779968bd3f374708c03d7813cfeb2d549
chiu <hzy844376661@yahoo.com>
2024-12-17 09:54:50 +0900

    版本提交

--- src/plugins/globalRequest.ts
+++ src/plugins/globalRequest.ts
@@ -12,7 +12,7 @@ import {stringify} from "querystring";
  */
 const request = extend({
   credentials: 'include', // 默认请求是否带上cookie
-  prefix: process.env.NODE_ENV === 'production' ? 'http://43.207.229.111:8080' : undefined
+  prefix: process.env.NODE_ENV === 'production' ? 'http://gunlab-nav.com' : undefined
   // requestType: 'form',
 });
 
